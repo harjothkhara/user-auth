5cd562e10edeeee4e38a2d68f0728b23e7156f5b
harjoth <harjoth.khara@gmail.com>
2020-02-05 11:59:03 -0800

    testing async function again in users model to see if it works

--- users/users-model.js
+++ users/users-model.js
@@ -15,14 +15,22 @@ function findBy(filter) {
   return db('users').where(filter);
 }
 
-function add(user) {
-  return db('users')
-    .insert(user, 'id')
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
+async function add(user) {
+  const [id] = await db('users').insert(user, 'id');
+  console.log(id);
+
+  return findById(id);
 }
+
+// function add(user) {
+//   return db('users')
+//     .insert(user, 'id')
+//     .then(ids => {
+//       const [id] = ids;
+//       console.log(id);
+//       return findById(id);
+//     });
+// }
 
 function findById(id) {
   return db('users')
