0ea32b701e660d3124110a05d0b6fb89b7cc67c9
harjoth <harjoth.khara@gmail.com>
2020-02-05 11:28:54 -0800

    changed development to sqlite

--- knexfile.js
+++ knexfile.js
@@ -11,11 +11,20 @@ const pgDb = process.env.PG_DB;
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: localPgConnection,
+    client: 'sqlite3',
+    connection: {
+      filename: './database/auth.db3'
+    },
+    useNullAsDefault: true,
     migrations: { directory: './database/migrations' },
-    seeds: { directory: './database/seeds' }
+    seeds: { directory: './database/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); //enforce FK
+      }
+    }
   },
+
   testing: {
     client: 'sqlite3',
     connection: {
@@ -34,15 +43,16 @@ module.exports = {
       }
     }
   },
+
   production: {
-    client: 'pg', // npm i pg
-    connection: process.env.DATABASE_URL + '?ssl=true'
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
-      // config connections between app and server
       min: 2,
       max: 10
     },
     migrations: {
+      tableName: 'knex_migrations',
       directory: './database/migrations'
     },
     seeds: {
